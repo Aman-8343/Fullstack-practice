54dc090a5bb33fe9d57cb5d523297e8e092f1eee
Aman kumar <amankumar105541@gmail.com>
2025-03-21 21:45:17 +0530

    use axios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,37 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
+import { useEffect } from 'react'
 
 function App() {
   const [jokes, setJokes] = useState([])
 
+  useEffect(()=>{
+    axios.get('http://localhost:3000/jokes')
+    .then((Response)=>{
+      setJokes(Response.data)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  })
+
   return (
     <>
       <h1>Hero</h1>
+      <p>JOKES:{jokes.length}</p>
+
+      {
+        jokes.map((jokes,index)=>{
+          <div key={jokes.id}>
+            <h3>{jokes.title}+
+
+            </h3>
+
+          </div>
+        })
+      }
     </>
   )
 }
