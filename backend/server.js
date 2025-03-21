6ddfdf2bbb1f48cce080e3d1fbf42e844dfcc90d
Aman kumar <amankumar105541@gmail.com>
2025-03-21 21:07:06 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,21 @@ app.get('/',(req,res)=>{
     res.send('server is ready');
 })
 
+app.get('/jokes',(req,res)=>{
+    const jokes=[
+        {
+        id:1,
+        title: 'a joke'
+        },
+        {
+            id:2,
+            title:'another joke'
+
+        }
+    ];
+    res.send(jokes)
+})
+
 const port =process.env.PORT ||3000;
 
 app.listen(port,()=>{
